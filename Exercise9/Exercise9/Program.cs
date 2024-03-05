a118bda8ba6dacff535c429f64f2849734b65cdd
Philip-P-Sample-Text <145466967+Philip-P-Sample-Text@users.noreply.github.com>
2024-03-05 09:49:00 -0500

    Added SO many for loops!

---
+++ Exercise9/Exercise9/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Globalization;
+
+namespace Exercise9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowWidth;
+            int numberOfRows;
+
+            //This will parse the numbers that you wrote into the console.
+            Console.WriteLine("How many rows of asterisks do you want?");
+            numberOfRows = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What do you want the length of each row to be?");
+            rowWidth = int.Parse(Console.ReadLine());
+
+            //I put a for loop in a for loop, so you can for loop while you for loop.
+            //This will
+            for (int j = 0; j < numberOfRows; j++)
+            {
+
+                for (int i = 0; i < rowWidth; i++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+
+            int rows;
+            int addition = 0;
+
+            Console.WriteLine("How many rows of asterisks do you want?");
+            rows = int.Parse(Console.ReadLine());
+
+            for (int k = 0; k < rows; k++)
+            {
+
+                for (int y = rows + addition; y >= rows; y--)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+                addition++;
+            }
+        }
+    }
+}
